bf04e299227bd74e79556ddfd77454c8e1a21cc8
Chayma123-hub <chaymagargouch33@gmail.com>
2023-10-09 08:24:18 +0100

    initial git

---
+++ test1.py
@@ -0,0 +1,16 @@
+nom=input("Comment tu t'appelles :")
+
+prenom=input("Quel est ton nom de famille :")
+age=input("Quel âge as-tu?")
+date=input("Quelle est ta date de naissance :")
+robot=input("Es-tu une vraie personne ou es-tu un robot :")
+
+print("-----------Welcome to gmail------------")
+print("NOM = ",nom)
+print("PRENOM = ",prenom)
+print("AGE = ",age)
+print("DATE DE NAISSANCE = ",date)
+print("ROBOT OU HUMAIN = ",robot)
+
+
+
