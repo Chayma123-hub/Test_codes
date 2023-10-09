1c3873192a82c8e8e28446cd335640f395bc0a49
Chayma123-hub <chaymagargouch33@gmail.com>
2023-10-09 09:48:39 +0100

    remodification

--- test1.py
+++ test1.py
@@ -1,11 +1,11 @@
-nom=input("Comment tu t'appelles :")
+nom=input("Comment tu t'appelles ?")
 
-prenom=input("Quel est ton nom de famille :")
+prenom=input("Quel est ton nom de famille ?")
 age=input("Quel âge as-tu?")
 date=input("Quelle est ta date de naissance ?")
-robot=input("Es-tu une vraie personne ou es-tu un robot :")
+robot=input("Es-tu une vraie personne ou es-tu un robot ?")
 
-print("-----------Welcome to gmail------------")
+print("-----------Welcome TO GMAIL------------")
 print("name = ",nom)
 print("PRENOM = ",prenom)
 print("YOUR AGE = ",age)
